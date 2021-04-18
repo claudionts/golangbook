6c470cc0e5ecce5383b9588c0a4b3f9a641e3f5d
Claudio Neto <jneto@mcontigo.com>
2021-04-18 18:23:04 -0300

    iteracao

---
+++ iteracao/iteracao_test.go
@@ -0,0 +1,18 @@
+package iteracao
+
+import "testing"
+
+func TestRepetir(t *testing.T) {
+	repeticoes := Repetir("a")
+	esperando := "aaaaa"
+
+	if repeticoes != esperando {
+		t.Errorf("esperado '%s' mas obteve '%s'", esperando, repeticoes)
+	}
+}
+
+func BenchmarkRepetir(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+			Repetir("a")
+	}
+}
